801ae51e0b2646909ecca5fbe5e29c1c7ab796c6
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-28 01:39:44 +0900

    Update after.sh

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKER_ID=$(sudo docker ps |awk '{print $1}')
+DOCKER_ID=$(sudo docker ps |grep "iplass" |awk '{print $1}')
 
 sudo docker cp /etc/codeDeploy/iplass_front/. $DOCKER_ID:/var/www/html/iplass_front
 
