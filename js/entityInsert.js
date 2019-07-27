fa67eb0c0872d5f3820374d1c5b5ad85ece79a4d
michaerel_0611 <michaerel_0611@yahoo.co.jp>
2019-07-27 23:59:03 +0900

    test

---
+++ js/entityInsert.js
@@ -0,0 +1,44 @@
+$('form').submit(function(){
+  var formData = $("form").serializeArray();
+  var formJson = "{";
+  for (var i=0; i<formData.length; i++) {
+    formJson = formJson + formData[i].name+" : "+formData[i].value
+    if(i<formData.length-1){
+      formJson = formJson + " , "
+    }
+  }
+  formJson = formJson + " }"
+  alert (formJson);
+  
+  $.ajax({
+        type:"post",                // method = "POST"
+        url:"http://iplass3.ali3051.foobar.iplass.jp:8080/iplass/test_tenant/api/mtp/entity/test",  // POST送信先のURL
+        data:JSON.stringify(formJson),  // JSONデータ本体
+        contentType: 'application/json', // リクエストの Content-Type
+        dataType: "jsonp",           // レスポンスをJSONとしてパースする
+      　headers: {
+            'X-Auth-Id' : 'admin@test_tenant',
+            'X-Auth-Password' : '11111111',
+            'X-Requested-With' : 'XMLHttpRequest'
+        },
+        success: function(json_data) {   // 200 OK時
+            // JSON Arrayの先頭が成功フラグ、失敗の場合2番目がエラーメッセージ
+            if (!json_data[0]) {    // サーバが失敗を返した場合
+                alert("Transaction error. " + json_data[1]);
+                return;
+            }
+            // 成功時処理
+            location.reload();
+            alert(json_data[0]);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {         // HTTPエラー時
+            alert("Server Error. Pleasy try again later.");
+            console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+            console.log("textStatus     : " + textStatus);
+            console.log("errorThrown    : " + errorThrown.message);
+        },
+        complete: function() {      // 成功・失敗に関わらず通信が終了した際の処理
+            button.attr("disabled", false);  // ボタンを再び enableにする
+        }
+    });
+});
