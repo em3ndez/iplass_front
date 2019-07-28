e39763822ef71c6cbec5301e5e0ff7a0b2b506bc
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-29 00:08:07 +0900

    Update entityInsert.js

--- src/js/entityInsert.js
+++ src/js/entityInsert.js
@@ -1,11 +1,3 @@
-#if(typeof jQuery != "undefined"){ //jQueryの読み込み確認
-#    $(function(){
-#        alert('jQuery is ready.')
-#    });
-#}
-
-
-
 $('#ajax').on('click',function(){
 alert("ajax")
 });
