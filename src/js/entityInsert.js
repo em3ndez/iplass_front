9cb08c638c461dfc7d9b4dcdee53f3c7887ed9f4
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-29 01:21:44 +0900

    Update entityInsert.js

--- src/js/entityInsert.js
+++ src/js/entityInsert.js
@@ -1,3 +1,13 @@
 $(document).on('click', '#ajax', function(){
-  alert('Button is clicked');
+  $('form').submit(function(){
+  var formData = $("form").serializeArray();
+  var formJson = "{";
+  for (var i=0; i<formData.length; i++) {
+    formJson = formJson + formData[i].name+" : "+formData[i].value
+    if(i<formData.length-1){
+      formJson = formJson + " , "
+    }
+  }
+  formJson = formJson + " }"
+  alert (formJson);
 });
