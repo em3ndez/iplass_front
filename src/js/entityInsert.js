dd011393d3d35fab071b1fe091fd418f941dc5a7
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-29 16:24:41 +0900

    Update entityInsert.js

--- src/js/entityInsert.js
+++ src/js/entityInsert.js
@@ -32,3 +32,14 @@ $(document).on('click', '#ajax', function(){
         }
     });
 });
+
+
+$(document).on("click", ".add", function() {
+    $(this).parent().clone(true).insertAfter($(this).parent());
+});
+$(document).on("click", ".del", function() {
+    var target = $(this).parent();
+    if (target.parent().children().length > 1) {
+        target.remove();
+    }
+});
