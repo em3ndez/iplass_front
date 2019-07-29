d4bda71fc80fcee6dec5ea7bc76c8d3d367c6ff7
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-29 17:51:21 +0900

    Update entityInsert.js

--- src/js/entityInsert.js
+++ src/js/entityInsert.js
@@ -36,7 +36,7 @@ $(document).on('click', '#ajax', function(){
 
 $(document).on("click", ".add", function() {
     $("#form_1 tr:first").clone(true).insertAfter($("#form_1 tr:last"));
-    $("#form_1 tr:last").append('<td></td>').trigger("create");
+    $("#form_1 tr:last").append('<td></td>');
     $(".del:last").clone(true).attr('type','button').appendTo($("#form_1 td:last"));
 });
 $(document).on("click", ".del", function() {
