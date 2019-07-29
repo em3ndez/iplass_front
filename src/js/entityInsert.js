4f2c1054d7b617ed68522425e350ad58501d3efb
tanakahiroki <michaerel_0611@yahoo.co.jp>
2019-07-29 13:31:44 +0900

    Update entityInsert.js

--- src/js/entityInsert.js
+++ src/js/entityInsert.js
@@ -1,13 +1,9 @@
 $(document).on('click', '#ajax', function(){
   var formData = $("form").serializeArray();
-  var formJson = "{";
+  var formJson = {}
   for (var i=0; i<formData.length; i++) {
-    formJson = formJson + formData[i].name+" : "+formData[i].value
-    if(i<formData.length-1){
-      formJson = formJson + " , "
-    }
+    formJson.formData[i].name = formData[i].value
   }
-  formJson = formJson + " }"
   alert (formJson);
   
   $.ajax({
